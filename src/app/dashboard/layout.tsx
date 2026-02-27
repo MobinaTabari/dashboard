9002b822ed7ad0289d2b2954905e0a017f4a8f93
mobina1383tabari-beep <mobina.1383tabari@gmail.com>
2026-02-27 22:23:55 +0330

    style

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const dashboardLayout = () => {
+  return (
+    <div>
+        <link href='/'>Home</link>
+        <link href='/event'>event</link>
+    </div>
+  )
+}
+
+export default dashboardLayout
